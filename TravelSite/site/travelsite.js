4c4aa8f8d55955e0722e8f35e1a6e0e948e919a3
eddalosa <edmondalosa3@gmail.com>
2022-05-12 13:06:46 +0300

    Moved 'fortune cookie' into module.

--- TravelSite/site/travelsite.js
+++ TravelSite/site/travelsite.js
@@ -1,14 +1,8 @@
+const fortune  = require('./lib/fortune')
 const express = require('express')
 const expressHandlebars =  require('express-handlebars')
 const app = express()
 
-const fortunes  = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise",
-    "Whenever possible, keep it simple",
-]
 
 //configure Handlebars view engine
 
@@ -25,8 +19,7 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: randomFortune})
+    res.render('about', {fortune: getFortune() })
 })
 
 app.get('/', (req, res) => res.render('home'))
