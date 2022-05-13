5dd7c3b69270bc159988b495475fc05e8dd365c2
eddalosa <edmondalosa3@gmail.com>
2022-05-13 14:06:56 +0300

    add testing with jets and linting with eslint

--- TravelSite/site/travelsite.js
+++ TravelSite/site/travelsite.js
@@ -1,4 +1,5 @@
 const fortune  = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars =  require('express-handlebars')
 const app = express()
@@ -16,29 +17,21 @@ const port =  process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: getFortune() })
-})
-
-app.get('/', (req, res) => res.render('home'))
-
+app.get('/about', handlers.about)
 
 //custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 //custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost: ${port} ` +
-    `press Ctrl-C to terminate.`
-))
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.')
+    })
+
+} else {
+    module.exports = app
+}
